a9724c5a58c47e49ebf514ee4c5f40ede05c86ff
Koji E. Kosugi <kosugitti@gmail.com>
2024-02-05 15:24:46 +0900

    20240205

---
+++ YamaguchiR001/sticker.R
@@ -0,0 +1,15 @@
+library(magick)
+library(grid)
+library(tidyverse)
+library(hexSticker)
+
+imgurl <- "~/Dropbox/Git/slides/YamaguchiR001/YamaguchiR_logo_cut.png"
+
+sticker(imgurl, package="",
+        s_x = 1,
+        s_y = 1,
+        s_width = 0.8,
+        s_height = 0.8,
+        h_fill = "#ffff00", p_family = "gochi",p_color="#990099",
+        filename="YRstiker.png")
+
