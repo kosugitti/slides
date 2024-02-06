55a82f3dde8003430d39777366b5f48791770a67
Koji E. Kosugi <kosugitti@gmail.com>
2024-02-06 17:00:02 +0900

    20240206 とりあえずβ版かな

--- YamaguchiR001/sticker.R
+++ YamaguchiR001/sticker.R
@@ -5,11 +5,12 @@ library(hexSticker)
 
 imgurl <- "~/Dropbox/Git/slides/YamaguchiR001/YamaguchiR_logo_cut.png"
 
-sticker(imgurl, package="",
-        s_x = 1,
-        s_y = 1,
-        s_width = 0.8,
-        s_height = 0.8,
-        h_fill = "#ffff00", p_family = "gochi",p_color="#990099",
-        filename="YRstiker.png")
-
+sticker(imgurl,
+  package = "",
+  s_x = 1,
+  s_y = 1,
+  s_width = 0.8,
+  s_height = 0.8,
+  h_fill = "#ffff00", p_family = "gochi", p_color = "#990099",
+  filename = "YRstiker.png"
+)
